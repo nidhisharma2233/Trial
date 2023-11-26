b940c5b97622ed7368c55be8cf13f1c8fc20aff3
nidhi sharma <nidhis162001@gmail.com>
2023-11-26 15:32:57 +0530

    first commit

--- 23nov2023/assignments.py
+++ 23nov2023/assignments.py
@@ -29,58 +29,14 @@ class realworldapp:
                 self.json_read()
 
     def text_write(self):
-        user = int(input("1.list \n,2.dict\n3.set\nenter your choice for write"))
-        if user == 1:
-            n = int(input("Enter the number of elements: "))
-            user_list = []
-            for i in range(n):
-                element = input(f"Enter element {i + 1}: ")
-                user_list.append(element)
-            self.new = user_list
-
-        if user == 2:
-            user_input = input("Enter key-value pairs separated by commas (key:value format): ")
-            key_value_pairs = user_input.split(',')  # Splitting the input string by commas to get pairs
-            user_dict = {}
-
-            for pair in key_value_pairs:
-                key, value = pair.split(':')  # Splitting each pair by colon to get key and value
-                user_dict[key.strip()] = value.strip()
-            self.new = user_dict
-        if user==3:
-            input_values = input("Enter space-separated values to create a set: ")
-            values_list = input_values.split()
-            user_set = set(values_list)
-            self.new=user_set
+        self.new=input("enter your message=")
         f = open("myfile.txt", "w")
         f.write(self.new)
         f.close()
 
     def text_append(self):
-        user = int(input("1.list \n,2.dict\n3.set\nenter your choice for write"))
-        if user == 1:
-            n = int(input("Enter the number of elements: "))
-            user_list = []
-            for i in range(n):
-                element = input(f"Enter element {i + 1}: ")
-                user_list.append(element)
-            self.new = user_list
-
-        if user == 2:
-            user_input = input("Enter key-value pairs separated by commas (key:value format): ")
-            key_value_pairs = user_input.split(',')  # Splitting the input string by commas to get pairs
-            user_dict = {}
-
-            for pair in key_value_pairs:
-                key, value = pair.split(':')  # Splitting each pair by colon to get key and value
-                user_dict[key.strip()] = value.strip()
-            self.new = user_dict
-        if user == 3:
-            input_values = input("Enter space-separated values to create a set: ")
-            values_list = input_values.split()
-            user_set = set(values_list)
-            self.new = user_set
-        f = open("myfile.txt", "a")
+        self.new = input("enter your message=")
+        f = open("myfile.txt","a")
         f.write(self.new)
         f.close()
     def text_read(self):
@@ -115,7 +71,7 @@ class realworldapp:
         with open('csvfile.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(self.new)
-        file.close()
+
 
     def csv_append(self):
         user = int(input("1.list \n,2.dict\n3.set\nenter your choice for write"))
@@ -145,7 +101,7 @@ class realworldapp:
         with open('csvfile.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(self.new)
-        file.close()
+
 
     def csv_read(self):
         with open("csvfile.csv", 'r') as file:
@@ -173,7 +129,7 @@ class realworldapp:
             self.new=user_dict
         with open('json_demo.json', 'w') as f:
             json.dump(self.new, f)
-        f.close()
+
     def json_append(self):
         user = int(input("1.list \n,2.dict\n,enter your choice for append"))
         if user == 1:
@@ -196,13 +152,13 @@ class realworldapp:
 
         with open('json_demo.json', 'a') as f:
             json.dump(self.appen,f)
-        f.close()
+
 
     def json_read(self):
         with open('json_demo.json', 'r') as f:
             d = json.load(f)
             print(d)
-        f.close()
+
 
 
 obj=realworldapp()
